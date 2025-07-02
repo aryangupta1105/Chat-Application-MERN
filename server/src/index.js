5f70070eb91b51f23ba7df55403d93a34544ae3e
aryangupta1105 <husltegamingfanclub@gmail.com>
2025-07-02 21:42:27 +0530

    error deployment render

--- server/src/index.js
+++ server/src/index.js
@@ -68,9 +68,9 @@ if(process.env.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname , "../client/dist")));
 
 
-    app.get("*" , (req, res)=>{
-        res.sendFile(path.join(__dirname , "../client" , "dist" , "index.html"));
-    })
+    app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname , "../client" , "dist" , "index.html"));
+    });
 }
 
 
